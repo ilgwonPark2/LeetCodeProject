ac99aa1bd077a1f23ef608dce388250080a62786
danielPark1 <ilgwon@ds.seoultech.ac.kr>
2023-01-23 16:27:47 +0900

    Time: 161 ms (46.42%), Space: 52.6 MB (77.24%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -2,6 +2,36 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
+// const groupAnagrams = (strs) => {
+//     const checkAnagram = (word, comparingWord) => {
+//         const arrangedWord = word.split("").sort().join("")
+//         const arrangedComparingWord = comparingWord.split("").sort().join("")
+//         return arrangedWord === arrangedComparingWord
+//     }
+    
+//     return strs.reduce((acc,  cur) => {
+//         let tmp = acc
+//         if (acc.length === 0) {
+//             tmp.push([cur])
+//         } else {
+            
+//             Loop1: for (let i = 0; i < tmp.length; i++) {
+//                 const isAnagram = checkAnagram(tmp[i][0], cur)
+//                 if (isAnagram) {
+//                     tmp[i].push(cur)
+//                     break Loop1
+//                 } else if (i === tmp.length -1) {
+//                     tmp.push([cur])
+//                     break Loop1
+//                 }
+//             }
+//         }
+//         return tmp
+//     }, [])
+    
+//     // return answer
+    
+// };
 
 const groupAnagrams = (strs) => {
     let map = new Map();
@@ -11,6 +41,7 @@ const groupAnagrams = (strs) => {
         if (map.has(tmp)) map.get(tmp).push(s)
         else map.set(tmp,[s])
     }
+    
 
     return [...map.values()]
 
