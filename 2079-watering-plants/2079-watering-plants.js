6fb06096fe7875201705afda9ff0ffd1d15af1a8
danielPark1 <ilgwon@ds.seoultech.ac.kr>
2023-07-14 20:13:24 +0900

    Time: 65 ms (30.77%), Space: 42.6 MB (24.04%) - LeetHub

--- 2079-watering-plants/2079-watering-plants.js
+++ 2079-watering-plants/2079-watering-plants.js
@@ -9,16 +9,12 @@ const wateringPlants = function(plants, capacity) {
     
     for (let i = 0; i < plants.length; i++) {
         currentCapacity -= plants[i];
-        // console.log(i, count);
         if (currentCapacity < plants[i+1]) {
-            // console.log(count)
             count += (i - (-1)) * 2 +1;
-            // console.log(count)
             currentCapacity = capacity;
         } else {
             if (i !== plants.length -1) count++;
-        }
-        // console.log(i, count);
+        } 
     }
     return count
 };
