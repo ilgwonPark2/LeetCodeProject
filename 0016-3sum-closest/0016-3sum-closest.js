d77c0a170ce915d111bb2c5afaeac9b9bb464a41
danielPark1 <ilgwon@ds.seoultech.ac.kr>
2023-07-22 16:59:07 +0900

    Time: 81 ms (72.26%), Space: 43.7 MB (69.97%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.js
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -6,18 +6,13 @@
 const threeSumClosest = (nums, target) => {
     
     nums.sort((a,b) => a-b);
-    let closeVal = Infinity;
-    
     // check a difference bewteen two values
     const difference = (a, b) => Math.abs(a - b);
     
-    for (let i = 0; i < nums.length - 2; i++) {
-        
-//         while ()
-        // const threeSum = nums[i] + nums[i + 1] + nums[i + 2];
+    let closeVal = Infinity;
     
-//         // console.log('if', difference(target, threeSum),  threeSum, closeVal);
-//         if (difference(target, threeSum) < difference(target, closeVal)) closeVal = threeSum;
+    
+    for (let i = 0; i < nums.length - 2; i++) {
         let left = i + 1; right = nums.length - 1;
         
         while (left < right) {
